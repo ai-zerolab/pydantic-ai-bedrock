9444967544e4e181812a148aadfcf2e3816a3d0f
Wh1isper <jizhongsheng957@gmail.com>
2025-01-26 13:53:10 +0800

    Add py.typed and clean some code

--- pydantic_ai_bedrock/bedrock.py
+++ pydantic_ai_bedrock/bedrock.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import functools
-import json
 import typing
 from contextlib import asynccontextmanager
 from dataclasses import dataclass, field
@@ -17,7 +16,6 @@ from pydantic_ai.messages import (
     ModelResponse,
     ModelResponsePart,
     ModelResponseStreamEvent,
-    PartStartEvent,
     RetryPromptPart,
     SystemPromptPart,
     TextPart,
